1c79bf4743b3bb65e58635398d41fbaade5908d1
Alyssajin <alyssajin2019@gmail.com>
2023-12-30 14:29:21 -0800

    Stucking in elements.

--- proj1b/src/ArrayDeque.java
+++ proj1b/src/ArrayDeque.java
@@ -3,13 +3,39 @@ import java.util.List;
 
 public class ArrayDeque<T> implements Deque<T> {
 
-    /** Constructor*/
-    public ArrayDeque() {
+    /** Use AList to construct arrayDeque. */
+    private class AList<Item> {
+        private Item[] items;
+        private int size;
 
+        private AList() {
+            items = (Item[]) new Object[6];
+            size = 0;
+        }
+    }
+    /** Index of the next front item. */
+    private int nextFirst;
+    /** Index of the next last item. */
+    private int nextLast;
+    private AList p;
+
+
+    /** Constructor for an empty arrayDeque. */
+    public ArrayDeque() {
+        /* Initialise the nextFirst index && the nextLast index. */
+        nextFirst = 2;
+        nextLast = 3;
+    }
+
+    /** Check if the arrayDeque is full. */
+    public boolean isFull() {
+        return false;
     }
 
     @Override
     public void addFirst(T x) {
+
+        nextFirst += 1;
 
     }
 
@@ -50,6 +76,8 @@ public class ArrayDeque<T> implements Deque<T> {
 
     public static void main(String[] args) {
         Deque<Integer> ad = new ArrayDeque<>();
+        ad.addFirst(7);
+        ad.addFirst(25);
     }
 
 }
