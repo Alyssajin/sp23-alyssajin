b09052323a947014c3c01e7aa25ff57b3ceb7a9d
Alyssajin <alyssajin2019@gmail.com>
2023-12-30 13:54:44 -0800

    Set up for proj1b.

---
+++ proj1b/src/ArrayDeque.java
@@ -0,0 +1,55 @@
+import java.nio.charset.IllegalCharsetNameException;
+import java.util.List;
+
+public class ArrayDeque<T> implements Deque<T> {
+
+    /** Constructor*/
+    public ArrayDeque() {
+
+    }
+
+    @Override
+    public void addFirst(T x) {
+
+    }
+
+    @Override
+    public void addLast(T x) {
+
+    }
+
+    @Override
+    public List<T> toList() {
+        return null;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public T removeFirst() {
+        return null;
+    }
+
+    @Override
+    public T removeLast() {
+        return null;
+    }
+
+    @Override
+    public T get(int index) {
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Deque<Integer> ad = new ArrayDeque<>();
+    }
+
+}
