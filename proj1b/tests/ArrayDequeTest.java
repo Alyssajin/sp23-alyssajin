6992d9d7c46b98c786280033173464eba3ea0a6d
Alyssajin <alyssajin2019@gmail.com>
2024-01-02 17:11:47 -0800

    Constructs getTest.

--- proj1b/tests/ArrayDequeTest.java
+++ proj1b/tests/ArrayDequeTest.java
@@ -64,4 +64,83 @@ public class ArrayDequeTest {
         assertWithMessage("This deque size should be 3.").that(ad.size()).isEqualTo(3);
     }
 
+    @Test
+    @DisplayName("Test 1: Initialise an empty deque and let it return the 1st item.")
+    /** This test checks the function of get(). If a correct item is returned by using get(int i) function. */
+    public void getTest01() {
+        Deque<Integer> ad = new ArrayDeque<>(); // []
+        assertWithMessage("Test 1: This deque should return null.").that(ad.isEmpty()).isEqualTo(true);
+    }
+
+    @Test
+    @DisplayName("Test 2: Passes an integer to the deque and let it return the 1st item.")
+    public void getTest02() {
+        Deque<Integer> ad = new ArrayDeque<>(); // []
+        ad.addFirst(3); // [3]
+        assertWithMessage("Test 2: This deque should return 3.").that(ad.get(0)).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("Test 3: Passes multiple integers to the deque and let it return the 2nd item.")
+    public void getTest03() {
+        Deque<Integer> ad = new ArrayDeque<>(); // []
+        ad.addFirst(3); // [3]
+        ad.addFirst(9); // [3, 9]
+        ad.addFirst(93); // [3, 9, 93]
+        ad.addLast(31); // [3, 9, 93, 31]
+        assertWithMessage("Test 3: This deque should return 9.").that(ad.get(1)).isEqualTo(9);
+    }
+
+    @Test
+    @DisplayName("Test 4: Passes multiple integers to the deque and let it return the 2nd item.")
+    public void getTest04() {
+        Deque<Integer> ad = new ArrayDeque<>(); // []
+        ad.addFirst(3); // [3]
+        ad.addFirst(9); // [3, 9]
+        ad.addFirst(93); // [3, 9, 93]
+        ad.addLast(31); // [3, 9, 93, 31]
+        assertWithMessage("Test 4: This deque should return null.").that(ad.get(49)).isEqualTo(null);
+    }
+
+    @Test
+    @DisplayName("Test 5: Initialise an empty string type deque and let it return the 7st item.")
+    public void getTest05() {
+        Deque<String> ad = new ArrayDeque<>(); // []
+        assertWithMessage("Test 5: This deque should return null.").that(ad.get(6)).isEqualTo(null);
+    }
+
+    @Test
+    @DisplayName("Test 6: Passes a string to the deque and let it return the 1st item.")
+    public void getTest06() {
+        Deque<String> ad = new ArrayDeque<>(); // []
+        ad.addFirst("Alyssa"); // ["Alyssa"]
+        assertWithMessage("Test 6: This deque should return Alyssa.").that(ad.get(0)).isEqualTo("Alyssa");
+    }
+
+    @Test
+    @DisplayName("Test 7: Passes multiple strings to the deque and let it return the 3rd item.")
+    public void getTest07() {
+        Deque<String> ad = new ArrayDeque<>(); // []
+        ad.addFirst("Alyssa"); // ["Alyssa"]
+        ad.addFirst("Yoongi"); // ["Yoongi", "Alyssa"]
+        ad.addFirst("Confidence"); // ["Confidence", "Yoongi", "Alyssa"]
+        ad.addLast("Calm"); // ["Confidence", "Yoongi", "Alyssa", "Calm"]
+        assertWithMessage("Test 7: This deque should return 9.").that(ad.get(2)).isEqualTo("Alyssa");
+    }
+
+    @Test
+    @DisplayName("Test 8: let the deque return the 75th item and it should return null.")
+    public void getTest08() {
+        Deque<String> ad = new ArrayDeque<>(); // []
+        ad.addFirst("Alyssa"); // ["Alyssa"]
+        assertWithMessage("Test 8: This deque should return null.").that(ad.get(74)).isEqualTo(null);
+    }
+
+    @Test
+    @DisplayName("Test 9: let the deque return the -75th item and it should return null.")
+    public void getTest09() {
+        Deque<String> ad = new ArrayDeque<>(); // []
+        ad.addFirst("Alyssa"); // ["Alyssa"]
+        assertWithMessage("Test 9: This deque should return null.").that(ad.get(-74)).isEqualTo(null);
+    }
 }
